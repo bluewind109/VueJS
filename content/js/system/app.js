25422a45a2eb602c7debf1278dc94dd34ec440c9
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2018-09-19 17:50:55 +0700

    keyboard event and 2-way data binding

--- content/js/system/app.js
+++ content/js/system/app.js
@@ -1,32 +1,13 @@
 new Vue({
-  el: '.vue-app',
+  el: '#vue-app',
   data: {
-    name: 'FlyK',
-    job: 'Web Developer',
-    age: 25,
-    website: 'http://www.google.com.vn/',
-    websiteTag: '<a href="http://www.google.com.vn/">Google</a>',
-    x: 0,
-    y: 0
+    name: '',
+    age: ''
   },
   methods: {
-    greet: function(time) {
-      return 'Good ' + time + ' ' + this.name;
+    logName: function() {
     },
-    add: function(number) {
-      this.age += number;
-    },
-    subtract: function(number) {
-      if (this.age - number >= 0) {
-        this.age -= number;
-      }
-    },
-    updateXY: function(event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    click: function() {
-      alert('Clicked');
+    logAge: function() {
     }
   }
 });
