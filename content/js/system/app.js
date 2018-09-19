e1e4a18fc3d4e915c09451f03af2d70a804d32dd
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2018-09-19 17:32:15 +0700

    VueJS bind and mouse click event

--- content/js/system/app.js
+++ content/js/system/app.js
@@ -1,3 +1,32 @@
 new Vue({
-  el: '#vue-app'
+  el: '.vue-app',
+  data: {
+    name: 'FlyK',
+    job: 'Web Developer',
+    age: 25,
+    website: 'http://www.google.com.vn/',
+    websiteTag: '<a href="http://www.google.com.vn/">Google</a>',
+    x: 0,
+    y: 0
+  },
+  methods: {
+    greet: function(time) {
+      return 'Good ' + time + ' ' + this.name;
+    },
+    add: function(number) {
+      this.age += number;
+    },
+    subtract: function(number) {
+      if (this.age - number >= 0) {
+        this.age -= number;
+      }
+    },
+    updateXY: function(event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
+    },
+    click: function() {
+      alert('Clicked');
+    }
+  }
 });
