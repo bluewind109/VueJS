3aba8aae28bcd008e289bc48da5b96345b0dd56d
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2018-09-20 13:41:00 +0700

    Components

--- content/js/system/app.js
+++ content/js/system/app.js
@@ -1,33 +1,39 @@
+Vue.component('greeting', {
+  template: '<p>Hey there, I am {{name}} <button @click="changeName">Chane name</button></p>',
+  data: function() {
+    return {
+      name: 'Yoshi'
+    }
+  },
+  methods: {
+    changeName: function() {
+      this.name = 'Mario';
+    }
+  }
+});
+
 var appOne = new Vue({
   el: '#vue-app-one',
   data: {
-    title: 'Vue App One'
+
   },
   methods: {
 
   },
   computed: {
-    greet: function() {
-      return 'Hello from app one'
-    }
+
   }
 });
 
 var appTwo = new Vue({
   el: '#vue-app-two',
   data: {
-    title: 'Vue App Two'
+
   },
   methods: {
-    changeTitle: function() {
-      appOne.title = 'Title App One Changed';
-    }
+
   },
   computed: {
-    greet: function() {
-      return 'Greetings from app two'
-    }
+
   }
 });
-
-appTwo.title = 'Outside change';
