26d13b896ed7a5d0b7ae75ec2fb75ebc7e79e158
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2018-09-20 10:21:06 +0700

    v-for lesson

--- content/js/system/app.js
+++ content/js/system/app.js
@@ -1,18 +1,25 @@
 new Vue({
   el: '#vue-app',
   data: {
-    available: false,
-    nearby: false
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+    ninjas: [{
+        name: 'Ryu',
+        age: 25
+      },
+      {
+        name: 'Yoshi',
+        age: 35
+      },
+      {
+        name: 'Ken',
+        age: 55
+      }
+    ]
   },
   methods: {
 
   },
   computed: {
-    compClasses: function() {
-      return {
-        available: this.available,
-        nearby: this.nearby
-      }
-    }
+
   }
 });
